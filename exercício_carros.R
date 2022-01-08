49687d1f61cd001acd3e9565c859044e38298b3c
Goncalves <kgoncalves@gpstrategies.com>
2022-01-08 13:53:54 -0300

    segunda versão

--- exercício_carros.R
+++ exercício_carros.R
@@ -1,11 +1,17 @@
 # Versão 1
 # Versão 2
 
+library(dplyr)
+
 library(carData)
 carros <- cars
 head(carros)
+
+glimpse(carros)
+summary(carros)
 
 library(ggplot2)
 
 ggplot(data = carros, aes(x = dist, y = speed)) +
   geom_point()
+
